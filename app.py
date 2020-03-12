a0ca9b3e63b9b1293233d6e3aa17862c8f7c3c69
pradyumn jain <pradyumn25jain@gmail.com>
2020-03-12 15:31:03 +0530

    chack date validity

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+# from quickstart import set_google_reminder
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,6 +10,21 @@ api = Api(app)
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
+class checkdate(Resource):
+    def post(self):
+        parser =  reqparse.RequestParser()
+        parser.add_argument('date',type=str,required=True,help="cant be blank")
+        data = parser.parse_args()
+        date = data['date']
+        date = date.split(" ")
+        for dat in date:
+            try:
+                if type(int(dat)) == int:
+                    return {"validity":"valid"}
+            except:
+                continue
+        return {"validity":"invalid"}
 
 
 class testinfo(Resource):
